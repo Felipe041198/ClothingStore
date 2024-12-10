80d0aed5c3c70c053b1bb32ac8de0a9c3159ab3b
Felipe041198 <felipe041198@hotmail.com>
2024-12-10 16:46:54 -0300

    Update test_controlador_vendedores.py

--- tests/unittest/controller/test_controlador_vendedores.py
+++ tests/unittest/controller/test_controlador_vendedores.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
 
 from src.controller.controlador_sistema import ControladorSistema
 from src.controller.controlador_vendedores import ControladorVendedores
