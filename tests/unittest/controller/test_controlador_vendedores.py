b80cb4ebe957cb9e7181f999ae7fd1cb070ec39c
Felipe041198 <felipe041198@hotmail.com>
2024-12-10 14:34:06 -0300

    Atualiza os testes unitarios de vendedor

--- tests/unittest/controller/test_controlador_vendedores.py
+++ tests/unittest/controller/test_controlador_vendedores.py
@@ -16,10 +16,10 @@ class TestControladorVendedores(TestCase):
         self.controlador = ControladorVendedores(self.controlador_sistema)
 
     # Teste para o cadastro de vendedores
-    @patch('src.view.tela_vendedores.TelaVendedores.sucesso_cadastro')
-    @patch('src.view.tela_vendedores.TelaVendedores.obter_dados_vendedor')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.sucesso_cadastro')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.obter_dados_vendedor')
     def test_cadastrar_vendedor(self, mock_obter_dados_vendedor, mock_sucesso_cadastro):
-        mock_obter_dados_vendedor.return_value = vendedor1.to_dict()
+        mock_obter_dados_vendedor.return_value = vendedor1.to_dict(), False
 
         resultado = self.controlador.cadastrar_vendedor()
 
@@ -28,7 +28,7 @@ class TestControladorVendedores(TestCase):
         self.compara_vendedores(resultado, vendedor1)
 
     # Teste para a listagem de vendedores
-    @patch('src.view.tela_vendedores.TelaVendedores.exibir_vendedores')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.exibir_vendedores')
     def test_listar_vendedores(self, mock_exibir_vendedores):
         self.controlador.vendedores.append(vendedor1)
         self.controlador.vendedores.append(vendedor2)
@@ -39,7 +39,7 @@ class TestControladorVendedores(TestCase):
         self.assertEqual(len(resultado), 2)
 
     # Teste para listagem de vendedores vazia
-    @patch('src.view.tela_vendedores.TelaVendedores.mostrar_erro')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.mostrar_erro')
     def test_listar_vendedores_vazio(self, mock_mostrar_erro):
         result = self.controlador.listar_vendedores()
 
@@ -48,8 +48,8 @@ class TestControladorVendedores(TestCase):
         self.assertIsNone(result)
 
     # Teste para a busca de vendedores
-    @patch('src.view.tela_vendedores.TelaVendedores.obter_cpf')
-    @patch('src.view.tela_vendedores.TelaVendedores.exibir_vendedor')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.obter_cpf')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.exibir_vendedor')
     def test_busca_vendedor(self, mock_exibir_vendedor, mock_obter_cpf):
         self.controlador.vendedores.append(vendedor1)
         mock_obter_cpf.return_value = vendedor1.cpf
@@ -60,8 +60,8 @@ class TestControladorVendedores(TestCase):
         self.compara_vendedores(resultado, vendedor1)
 
     # Teste para a busca de vendedor não encontrado
-    @patch('src.view.tela_vendedores.TelaVendedores.mostrar_erro')
-    @patch('src.view.tela_vendedores.TelaVendedores.obter_cpf')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.mostrar_erro')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.obter_cpf')
     def test_busca_vendedor_nao_encontrado(self, mock_obter_cpf, mock_mostrar_erro):
         mock_obter_cpf.return_value = vendedor1.cpf
 
@@ -71,8 +71,8 @@ class TestControladorVendedores(TestCase):
         self.assertIsNone(resultado)
 
     # Teste para exclusão de vendedores
-    @patch('src.view.tela_vendedores.TelaVendedores.obter_cpf')
-    @patch('src.view.tela_vendedores.TelaVendedores.sucesso_exclusao')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.obter_cpf')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.sucesso_exclusao')
     def test_exclui_vendedor(self, mock_sucesso_exclusao, mock_obter_cpf):
         self.controlador.vendedores.append(vendedor1)
         mock_obter_cpf.return_value = vendedor1.cpf
@@ -83,8 +83,8 @@ class TestControladorVendedores(TestCase):
         mock_sucesso_exclusao.assert_called_once_with(vendedor1.nome)
 
     # Teste para exclusão de vendedor não encontrado
-    @patch('src.view.tela_vendedores.TelaVendedores.mostrar_erro')
-    @patch('src.view.tela_vendedores.TelaVendedores.obter_cpf')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.mostrar_erro')
+    @patch('src.view.tela_gui_vendedores.TelaVendedores.obter_cpf')
     def test_exclui_vendedor_nao_encontrado(self, mock_obter_cpf, mock_mostrar_erro):
         mock_obter_cpf.return_value = vendedor1.cpf
 
